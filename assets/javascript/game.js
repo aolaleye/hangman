84a9b88419e9ca1fcabbfd97b62e2e35403af037
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-02-21 22:14:54 -0500

    added an array of themes in js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-//Hides PLAY button for Game 1
+//<--- Hides PLAY button for Game 1 --->
 $("#play-1").click(function(){
     $("#get-started").hide();
     $("#play-1").hide();
@@ -7,11 +7,15 @@ $("#play-1").click(function(){
     $(".current-game").show();
 }) 
 
+var themes = ['"Landscape"', '"Space"', '"Holidays"'];
+var revealTheme1 = document.getElementsByClassName("theme").innerHTML = themes[0];
 
 
+
+
+
+// //<--- Reveals PLAY AGAIN button for Game 2 --->
 // $(".current-game").hide();
-
-// //Reveals PLAY AGAIN button for Game 2
 // $("#get-started").show();
 // $("play-2").show();
 // $("#play-2").click(function(){
