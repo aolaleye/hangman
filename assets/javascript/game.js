2cc25cf67a2ad4aa4ff02d189ce4075d86cce6fc
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-02-21 20:45:21 -0500

    included show() and hide() events

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+//Hides PLAY button for Game 1
+$("#play-1").click(function(){
+    $("#get-started").hide();
+    $("#play-1").hide();
+    //Reveals Game 1
+    $(".current-game").show();
+}) 
+
+
+
+// $(".current-game").hide();
+
+// //Reveals PLAY AGAIN button for Game 2
+// $("#get-started").show();
+// $("play-2").show();
+// $("#play-2").click(function(){
+//     $("#get-started").hide();
+//     $("#play-2").hide();
+//     //Reveals Game 2
+//     $(".current-game").show();
+// }) 
+
